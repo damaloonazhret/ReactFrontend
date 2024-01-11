6b2cf82c46826ab2b10c5c1aeddb363ed3ffc139
Andrey <poiprizamoda@gmail.com>
2024-01-11 19:41:55 +0300

    6: routing, react-router-dom, bundle to chunks (lazy loading, react suspense)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, {Suspense} from 'react';
 import './index.scss'
+import {Link, Route, Routes} from 'react-router-dom';
+import {AboutPageAsync} from "./pages/MainPage/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            Lorem ipsum dolor sit amet.
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
