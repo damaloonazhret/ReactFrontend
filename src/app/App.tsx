90020db7c70eea35404789d82efb2f18f1835ea6
Andrey <poiprizamoda@gmail.com>
2024-01-13 20:39:22 +0300

    9: FSD reafactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from 'react-router-dom';
-import {AboutPageAsync} from "./pages/MainPage/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage/MainPage.async";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
-
-
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -17,12 +15,11 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
     );
 };
-
 export default App;
