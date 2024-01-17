841cfd3ab9df8511420d9b79a60d768621cbca54
Andrey <poiprizamoda@gmail.com>
2024-01-17 19:07:13 +0300

    first tests, add new rule on eslint config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -6,6 +6,28 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: [
+        '\\\\node_modules\\\\',
+    ],
+    moduleDirectories: [
+        'node_modules',
+    ],
+    moduleFileExtensions: [
+        'js',
+        'mjs',
+        'cjs',
+        'jsx',
+        'ts',
+        'tsx',
+        'json',
+        'node',
+    ],
+    rootDir: '../../',
+    testMatch: [
+        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+    ],
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
@@ -16,8 +38,6 @@ const config: Config = {
     // cacheDirectory: "C:\\Users\\manager-pc\\AppData\\Local\\Temp\\jest",
 
     // Automatically clear mock calls, instances, contexts and results before every test
-    clearMocks: true,
-    testEnvironment: 'jsdom',
 
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
@@ -29,9 +49,6 @@ const config: Config = {
     // coverageDirectory: undefined,
 
     // An array of regexp pattern strings used to skip coverage collection
-    coveragePathIgnorePatterns: [
-        '\\\\node_modules\\\\',
-    ],
 
     // Indicates which provider should be used to instrument code for coverage
     // coverageProvider: "babel",
@@ -74,21 +91,8 @@ const config: Config = {
     // maxWorkers: "50%",
 
     // An array of directory names to be searched recursively up from the requiring module's location
-    moduleDirectories: [
-        'node_modules',
-    ],
 
     // An array of file extensions your modules use
-    moduleFileExtensions: [
-        'js',
-        'mjs',
-        'cjs',
-        'jsx',
-        'ts',
-        'tsx',
-        'json',
-        'node',
-    ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     // moduleNameMapper: {},
@@ -124,7 +128,6 @@ const config: Config = {
     // restoreMocks: false,
 
     // The root directory that Jest should scan for tests and modules within
-    rootDir: '../../',
 
     // A list of paths to directories that Jest should use to search for files in
     // roots: [
@@ -155,9 +158,6 @@ const config: Config = {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    testMatch: [
-        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
-    ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
