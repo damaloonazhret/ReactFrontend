0ea5f15245fad34242f39dae3beb9ec74b817ffd
Andrey <poiprizamoda@gmail.com>
2024-01-27 22:57:07 +0300

    fix linter

--- src/widgets/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/widgets/ui/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -10,9 +10,6 @@ const meta = {
         layout: 'centered',
     },
     tags: ['autodocs'],
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
 } satisfies Meta<typeof ThemeSwitcher>;
 
 export default meta;
